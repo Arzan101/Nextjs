bd39fc7282b7c63ffbb189e9d5cff96ad56ca33c
Arzan101 <azzuuu994@gmail.com>
2025-02-10 19:54:13 +0000

    main

--- React-crash-course/src/components/post.jsx
+++ React-crash-course/src/components/post.jsx
@@ -1,8 +1,12 @@
 
+const names = ['RAJA','DUBLIN'];
+
 function Post (){
+    const chosenName = Math.random() > 0.5 ? names[0]: names[1];
     return(
     <div>
         <h1>NIGGESHWAR </h1>
+        <h1>{chosenName}</h1>
     </div>
     );
 }
