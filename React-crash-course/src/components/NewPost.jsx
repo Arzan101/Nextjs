f2269750af3e2f6bad48ebdab142ee9bf86adca8
Arzan101 <azzuuu994@gmail.com>
2025-02-13 14:28:01 +0000

    main

--- React-crash-course/src/components/NewPost.jsx
+++ React-crash-course/src/components/NewPost.jsx
@@ -14,6 +14,7 @@ function NewPost() {
         <label htmlFor="body">Text</label>
         <textarea id="body" required rows={3} onChange={changeBodyHandler}/>
       </p>
+      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
