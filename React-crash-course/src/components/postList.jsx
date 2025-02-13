b0ad7aa4b2f32cbae1d92439b11ad5d804bc2747
Arzan101 <azzuuu994@gmail.com>
2025-02-13 15:21:27 +0000

    main

--- React-crash-course/src/components/postList.jsx
+++ React-crash-course/src/components/postList.jsx
@@ -1,13 +1,19 @@
 import NewPost from "./NewPost";
 import Post from "./post";
-import classes from "./postList.module.css"; // Make sure this file exists
+import classes from "./postList.module.css"; 
+import { useState } from "react";
 
 function PostList() {
+  const [enteredBody,setEnteredBody]= useState('');
+
+  function bodyChangeHandler(event){
+    setEnteredBody= event.target.value;
+  }
   return (
     <>
-    <NewPost/>
+    <NewPost onBodychange={bodyChangeHandler}/>
     <ul className={classes.posts}>
-      <Post author="Maximus" body="Dasimus" />
+      <Post author="Maximus" body={enteredBody} />
       <Post author="Maxi" body="Dasi" />
     </ul>
     </>
