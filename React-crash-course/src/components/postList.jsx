303a5f9eae4515ad3fdbce6ff521469defd40d1b
Arzan101 <azzuuu994@gmail.com>
2025-02-20 13:25:20 +0000

    main

--- React-crash-course/src/components/postList.jsx
+++ React-crash-course/src/components/postList.jsx
@@ -19,9 +19,15 @@ function PostList({isPosting,onStopPosting}) {
        <NewPost onCancel ={onStopPosting} onAddpost={addPostHandler}
        /> 
        </Modal>)}
-      <ul className={classes.posts}>
-        {posts.map((post)=> <Post author={post.author} body={post.body}/> )}
-      </ul>
+      {posts.length>0 && (<ul className={classes.posts}>
+        {posts.map((post)=> <Post key={post.body} author={post.author} body={post.body}/> )}
+      </ul>)}
+      {posts.length === 0 (
+        <div style={{textAlign:'center',color:'white'}}>
+        <h2>There are no Posts</h2>
+        <p>Start adding some!</p>
+        </div>
+      )}
     </>
   );
 }
